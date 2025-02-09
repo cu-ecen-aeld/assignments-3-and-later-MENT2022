ca721343a0136627dd0932a798f3cd98b8f2c75c
MENT2022 <mouin.eddeb@gmail.com>
2025-02-09 22:04:18 +0100

    Update assignment 4 Part 2 Instructions

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Tester script for assignment 1 and assignment 2
+# Tester script for assignment 4
 # Author: Mouin EDDEB
 
 set -e
@@ -8,63 +8,55 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)  # Absolute path for config
 
-if [ $# -lt 3 ]
-then
-	echo "Using default value ${WRITESTR} for string to write"
-	if [ $# -lt 1 ]
-	then
-		echo "Using default value ${NUMFILES} for number of files to write"
-	else
-		NUMFILES=$1
-	fi	
+if [ $# -lt 3 ]; then
+    echo "Using default value ${WRITESTR} for string to write"
+    if [ $# -lt 1 ]; then
+        echo "Using default value ${NUMFILES} for number of files to write"
+    else
+        NUMFILES=$1
+    fi
 else
-	NUMFILES=$1
-	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
+    NUMFILES=$1
+    WRITESTR=$2
+    WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
+assignment=$(cat /etc/finder-app/conf/assignment.txt)  # Absolute path for config
 
 echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
 
-# create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
-
-if [ $assignment != 'assignment1' ]
-then
-	mkdir -p "$WRITEDIR"
-
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
+if [ $assignment != 'assignment1' ]; then
+    mkdir -p "$WRITEDIR"
+    if [ -d "$WRITEDIR" ]; then
+        echo "$WRITEDIR created"
+    else
+        exit 1
+    fi
 fi
 
-for i in $( seq 1 $NUMFILES)
-do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+for i in $(seq 1 $NUMFILES); do
+    writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"  # Remove ./
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")  # Remove ./
 
-# remove temporary directories
+# Write result to required file
+echo "$OUTPUTSTRING" > /tmp/assignment4-result.txt
+
+# Cleanup
 rm -rf /tmp/aeld-data
 
 set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
+echo "${OUTPUTSTRING}" | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-	echo "success"
-	exit 0
+    echo "success"
+    exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-	exit 1
+    echo "failed: expected ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+    exit 1
 fi
